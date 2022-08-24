a62a71a2a18bc1ffe8bb0c53b24e83883ceb0fdf
Paul Schleifer <paul_schleifer@hotmail.com>
2022-08-24 12:47:34 +0100

    fix failing test & add more edge cases to basic solution

--- __tests__/index.test.ts
+++ __tests__/index.test.ts
@@ -11,9 +11,9 @@ class SimpleClass {
 
 const TestNumber = 42;
 
-describe("Container can handle registration and use of simple objects", () => {
+describe("Container can handle registration and use of simple values", () => {
 
-  test("store simple object", () => {
+  test("store and retrieve simple object", () => {
     const c = new Container();
 
     c.register('simple', new SimpleClass(TestNumber));
@@ -21,6 +21,31 @@ describe("Container can handle registration and use of simple objects", () => {
     const simpleObject = c.retrieve('simple') as SimpleClass;
 
     expect(simpleObject.count).toBe(TestNumber);
-  })
+  });
+
+  test("store and retrieve number", () => {
+    const c = new Container();
+
+    c.register('number', TestNumber);
+
+    expect(c.retrieve('number')).toBe(TestNumber);
+  });
+
+  test("store and retrieve a function", () => {
+    const c = new Container();
+    const fn = () => TestNumber;
+
+    c.register('func', fn);
+
+    const retrievedFunction = c.retrieve('func') as Function;
+
+    expect(retrievedFunction()).toBe(TestNumber);
+  });
+
+  test("retrieving nonexistant key yields 'undefined", () => {
+    const c = new Container();
+
+    expect(c.retrieve('does not exist')).toBeUndefined();
+  });
 
 });
--- src/Container/Container.ts
+++ src/Container/Container.ts
@@ -1,8 +1,16 @@
 export class Container {
+  private _services = new Map<string, unknown>();
 
-  register(key: string, value: unknown) {}
+  register(key: string, value: unknown) {
+    if (!this._services.has(key) && value !== undefined) {
+      this._services.set(key, value);
+    }
+  }
 
   retrieve (key: string): unknown {
-    return null;
+    // will retrieve 'undefined' if key is absent - should we throw an error?
+    if (this._services.has(key)) {
+      return this._services.get(key);
+    }
   }
 }
